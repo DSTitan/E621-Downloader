8aca63ce6a5ee28aa682fa8531e0f33d6a28b063
Nathan Torrezao <72593433+DSTitan@users.noreply.github.com>
2023-04-26 22:49:13 -0400

    Update 1.5.1

--- src/utility/utils/program.data.ts
+++ src/utility/utils/program.data.ts
@@ -1,6 +1,6 @@
 const Data = {
     name: "Fluffy Fetcher",
-    version: "1.5.0",
+    version: "1.5.1",
     description: "Effortlessly download and manage e926 and e621 posts in bulk, based on tags. Simplify your search and save time with this user-friendly program.",
 };
 
